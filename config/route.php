40f6f25984167c241174b0c04de4a9894098eb9f
Andres Jimenez <andresmauricio@protonmail.com>
2022-04-08 20:08:01 -0500

    Little changes in the 'config/route.php file was done, to display error when the user request is not found.

--- config/route.php
+++ config/route.php
@@ -20,11 +20,28 @@ $route = $dispatcher->dispatch(
     $_SERVER['REQUEST_URI']
 );
 
-// Here we use the array stored in '$route' in its position [1] and separate
-// its content in '$class' and '$method'.
-// https://www.php.net/manual/es/function.list.php
-list($class, $method) = $route[1];
+// Here we analyze the results in '$route' in its position [0] to validate if
+// the request was found or not.
+switch ($route[0]) {
 
-// We call '$class', '$method' and the results from '$route' array in tis
-// position [2]. https://www.php.net/manual/es/function.call-user-func-array
-call_user_func_array([new $class, $method], $route[2]);
+    case FastRoute\Dispatcher::NOT_FOUND:
+        echo 'Error 404';
+        break;
+
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        echo 'Error 405';
+        break;
+
+    case FastRoute\Dispatcher::FOUND:
+
+        // Here we use the array stored in '$route' in its position [1] and separate
+        // its content in '$class' and '$method'.
+        // https://www.php.net/manual/es/function.list.php
+        list($class, $method) = $route[1];
+
+        // We call '$class', '$method' and the results from '$route' array in tis
+        // position [2]. https://www.php.net/manual/es/function.call-user-func-array
+        call_user_func_array([new $class, $method], $route[2]);
+        break;
+
+}
